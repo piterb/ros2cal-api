475ae8b49f8bf1ebf083e7057553ba9597edc0f6
peter <peter.bodi@yahoo.com>
2025-12-15 13:11:33 +0100

    Initial commit

---
+++ src/main/java/com/ryr/ros2cal_api/Ros2calApiApplication.java
@@ -0,0 +1,13 @@
+package com.ryr.ros2cal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ros2calApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ros2calApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryr/ros2cal_api/Ros2calApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryr.ros2cal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ros2calApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
