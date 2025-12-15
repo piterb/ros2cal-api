3af440ed4a8d6e791cf62b0cc3a6cae3eabc7676
peter <peter.bodi@yahoo.com>
2025-12-15 13:59:41 +0100

    added roster controller

---
+++ src/main/java/com/ryr/ros2cal_api/RosterController.java
@@ -0,0 +1,16 @@
+package com.ryr.ros2cal_api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/roster")
+public class RosterController {
+
+    @GetMapping("/convert")
+    public ResponseEntity<String> convertRoster() {
+        return ResponseEntity.ok("Sample roster conversion");
+    }
+}
